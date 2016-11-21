e0c1087bc2ce7a056bcdbc384527492c27c7f7be
Florian Ducret <florian.ducret@hotmail.fr>
2016-11-21 17:14:06 +0100

    Dessin des Enums

--- src/dessin/EnumDessin.java
+++ src/dessin/EnumDessin.java
@@ -1,10 +1,29 @@
 package dessin;
 
+import general.Type;
+
 public class EnumDessin implements ElementDeDessin {
+	
+	private Type enu;
+	
+	public EnumDessin(Type enu){
+		this.enu = enu;
+	}
 
 	@Override
 	public void dessiner() {
 		// TODO Auto-generated method stub
+		String attributs = "Attributs : ";
+		
+		for(int i=0; i<enu.getDescription().getAttributs().size();i++){
+			attributs += enu.getDescription().getAttributs().get(i).getNom().toUpperCase() +"/n";
+		}
+		
+		System.out.println("/n -------------------------------------- /n" 
+				+ "Enum : " + enu.getNom() + "/n -------------------------------------- /n" 
+				+ "Package : " + enu.getPackage() + "/n -------------------------------------- /n"
+				+ attributs + "-------------------------------------- /n"
+				);
 
 	}
 
