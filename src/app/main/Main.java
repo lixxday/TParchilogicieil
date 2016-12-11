da6dba094167f9854a0367f614c0eb734591061a
corentinnormant <corentin.normant@gmail.com>
2016-12-11 17:57:21 +0100

    obsolete

--- src/app/main/Main.java
+++ src/app/main/Main.java
@@ -3,10 +3,9 @@ package app.main;
 import app.base.Graphique;
 import app.representation.Dessineur;
 import app.representation.RetourConsole;
-import app.representation.RetourSVG;
-import app.test.maClasse1;
-import app.test.maClasse2;
-import app.test.monInterface1;
+import app.test.MaClasse1;
+import app.test.MaClasse2;
+import app.test.MonInterface1;
 import app.visiteur.ClassRepresentation;
 import app.visiteur.IVisiteur;
 import app.visiteur.InterfaceRepresentation;
@@ -16,9 +15,6 @@ public class Main {
 
 	public static void main(String[] args) {
 
-//		ClassRepresentation maClasse1 = new ClassRepresentation(MaClasse1.class);
-//		ClassRepresentation maClasse2 = new ClassRepresentation(MaClasse2.class);
-//		InterfaceRepresentation monInterface1 = new InterfaceRepresentation(MonInterface1.class);
 		IVisiteur visiteur = new Visiteur();
 		Graphique graph = new Graphique();
 		Dessineur dessineur = new Dessineur(graph);
@@ -31,12 +27,8 @@ public class Main {
 		ClassRepresentation maClasse2 = new ClassRepresentation(MaClasse2.class);
 		graph.addGraphique(visiteur.visit(maClasse2));
     
-    InterfaceRepresentation monInterface1 = new InterfaceRepresentation(MonInterface1.class);
+		InterfaceRepresentation monInterface1 = new InterfaceRepresentation(MonInterface1.class);
 		graph.addGraphique(visiteur.visit(monInterface1));
-		
-		Dessineur dessineur = new Dessineur(graph);
-		RetourSVG retour = new RetourSVG();
-
 		
 		dessineur.visit(retour);
 	}
